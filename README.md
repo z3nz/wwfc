4ddc1b77de5387e1e6ebb557cd0a91930aaf2a36
Adam Lynch <adamlynchmob@gmail.com>
2017-09-21 17:02:25 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Words With Friends Cheat CLI
+CLI for finding words to play in words with friends or scrabble.
+
+<p align="center">
+ <img src="https://github.com/z3nz/wwfc/raw/master/assets/usage.png" width="600">
+</p>
+
+## Install
+```
+$ npm i wwfc -g
+```
+
+
+## Usage
+<p align="center">
+ <img src="https://github.com/z3nz/wwfc/raw/master/assets/help.png" width="600">
+</p>
---
+++ wwfc.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+const prog = require('caporal')
+const chalk = require('chalk')
+const dict = require('./dictionary')
+
+prog
+  .version('1.0.0')
+  .description('A CLI for cheating in words with friends and scrabble.')
+  .argument('<letters>', 'Letters to search with.')
+  .argument('[length]', 'Length of the word to search for.')
+  .action(function(args, options, logger) {
+    let letters = args.letters.split('')
+    const min = args.length || 2
+    const max = args.length || letters.length
+
+    const letterCount = { '*': 0 }
+    letters.forEach(letter => {
+      letterCount[letter] = letterCount[letter] ? letterCount[letter] + 1 : 1
+    })
+
+    const words = []
+    dict.forEach(word => {
+      if (word.length <= max && word.length >= min) {
+        let add = true
+        let wild = 0
+        word.split('').forEach(letter => {
+          if (!letters.includes(letter) && letterCount['*'] == wild) {
+            add = false
+          } else if (!letters.includes(letter)) {
+            wild++
+          } else if (word.split('').filter(l => l == letter).length > letterCount[letter]) {
+            add = false
+          }
+        })
+        if (add) words.push(word)
+      }
+    })
+
+    const sorted = {}
+    words.forEach(word => {
+      sorted[`${word.length}`] ? sorted[`${word.length}`].push(word) : sorted[`${word.length}`] = [word]
+    })
+
+    Object.keys(sorted).reverse().forEach(key => {
+      console.log(chalk.red.bold(`${key} letter words`))
+      console.log(sorted[key].join(' '))
+      console.log()
+    })
+  })
+
+prog.parse(process.argv)
